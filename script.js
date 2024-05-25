d8fad08daf41b1aa3ab322d191051289b7de4020
Rohith <rohithofficial10@gmail.com>
2024-05-25 23:11:00 +0530

    Added the basic logic there is a 16x16 grid which will change color on hover

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("hello");
+const container = document.getElementById("container");
+for (let i=0;i<256;i++) {
+    const el = document.createElement("div");
+    const value = 816/16;
+    el.style.width = `${value}px`;
+    el.style.height = `${value}px`;
+    el.onmouseover = () => el.style.backgroundColor = "red";
+    container.appendChild(el);
+} 
